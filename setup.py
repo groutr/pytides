b7f6cd6ef49df1ea7207b083fbdb3b8215a95a88
Ryan Grout <ryan.grout@noaa.gov>
2021-11-01 15:43:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(name='pytides',
       description='Tidal analysis and prediction library.',
-      version='0.0.6',
+      version='0.0.7',
       author='Sam Cox',
       author_email='sam.cox@cantab.net',
       url='http://github.com/sam-cox/pytides',
       packages=['pytides'],
-      install_requires=['numpy>=1.8','scipy>=0.11'],
+      install_requires=['numpy>=1.8','scipy>=1.0'],
       license='MIT')
